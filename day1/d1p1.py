6cc3644d9c57be7265b6383d43d72ffbc70fd8e9
nickb14 <nbilsborrow@gmail.com>
2021-12-05 06:23:59 +0000

    finished d1p1 2021 yay I code

--- day1/d1p1.py
+++ day1/d1p1.py
@@ -1,1 +1,16 @@
-print("hello world")
+nums = []
+
+with open("/workspace/AdventOfCode2021/day1/input.txt") as f:
+    line = f.readline()
+    while line:
+        nums.append(int(line))
+        line = f.readline()
+
+tot = -1
+prev = 0 #just start with this because total starts at -1 (supposed to ignore first value)
+for num in nums:
+    if num > prev:
+        tot += 1
+    prev = num
+
+print(tot)
