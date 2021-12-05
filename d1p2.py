49727ef6fd375377e5f0cfac80fe93e5a402604b
nickb14 <nbilsborrow@gmail.com>
2021-12-05 06:44:38 +0000

    finished d1p2 !! (idk how to do github correctly)

---
+++ d1p2.py
@@ -0,0 +1,19 @@
+nums = []
+with open("/workspace/AdventOfCode2021/day1/input.txt") as f:
+    line = f.readline()
+    while line:
+        nums.append(int(line))
+        line = f.readline()
+
+windows = []
+for i in range(len(nums)-2):
+    windows.append(nums[i] + nums[i+1] + nums[i+2])
+
+tot = -1
+prev = 0 #just start with this because total starts at -1 (supposed to ignore first value)
+for num in windows: #same logic as p1, just with windows
+    if num > prev:
+        tot += 1
+    prev = num
+
+print(tot)
