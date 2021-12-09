9ed623249ea5da4ff7ecc2adfdc4e0172fba01d7
nickb14 <nbilsborrow@gmail.com>
2021-12-09 17:16:32 +0000

    finished d4p1 (scuffed?)

--- day4/d4p1.py
+++ day4/d4p1.py
@@ -1,5 +1,6 @@
 def main():
 
+    boards = []
     with open("/workspace/AdventOfCode2021/day4/input.txt") as f:
         nums = f.readline()
         line = f.readline()
@@ -8,21 +9,39 @@ def main():
             for i in range(5):
                 line = f.readline()
                 board.append(line)
-            #make board into a list of 10 sets of 5
-            #put list into longr list? (or should I be uning lists idk)
+            boards.append(boardListToList(board))
             line = f.readline()
     
-    print(board)
-    print(boardListToList(board))
+    nums = list(map(int, nums.split(','))) #fancy~
+    
+    #nums = list of ints, the numbers in the order that they are "called"
+    #boards = list of lists of lists of ints, the boards represendted as all 10 of their win conditions
+    
+    def findWinner(): #why is this allowed though (I made a fuction to break out of the nested loops)
+        for num in nums:
+            for board in boards:
+                for row in board:
+                    if row.count(num) == 1:
+                        row.remove(num)
+                        if len(row) == 0:
+                            sumUnmarked = 0
+                            for i in range(5): #if we took all 10 rows there would be duplicates
+                                sumUnmarked += sum(board[i])
+                            return num * sumUnmarked
+    score = findWinner()
+    print(score)
 
-#this system very much does not work right now
-def boardListToList(boardStr): #list of strings to list of lists of ints
+def boardListToList(boardStr): #board: list of strings to list of lists of ints
     boardList = []
-    for row in boardStr:
-        boardList.append(rowStringToList(row))
+    for i in range(5): #for the verticals
+        boardList.append(list())
+    for rowStr in boardStr:
+        boardList.append(rowStringToList(rowStr)) #horizontals
+        for i in range(5):
+            boardList[i].append(int(rowStr[i*3:i*3+2])) #verticals
     return boardList
 
-def rowStringToList(rowStr): #string to list of ints
+def rowStringToList(rowStr): #row: string to list of ints
     rowList = []
     for i in range(5):
         rowList.append(int(rowStr[i*3:i*3+2]))
