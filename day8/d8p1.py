d03eee3adee784610e40bbf08ae944c2c82d41e2
nickb14 <nbilsborrow@gmail.com>
2021-12-23 03:31:38 +0000

    d8p1 done

---
+++ day8/d8p1.py
@@ -0,0 +1,20 @@
+def main():
+
+    total = 0
+
+    with open("/workspace/AdventOfCode2021/day8/input.txt") as f:
+        line = f.readline()
+        while line:
+
+            i = line.index('|') + 2 #oddly placed logic but whatever
+            outputs = line[i:].split()
+            for o in outputs:
+                s = len(o)
+                if s != 5 and s != 6:
+                    total += 1
+
+            line = f.readline()
+    
+    print(total)
+
+main()
