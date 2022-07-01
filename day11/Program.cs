ee8078a35788dffb31ed73699d82cf2757734ce8
nickb14 <nbilsborrow@gmail.com>
2022-06-30 22:20:41 -0700

    c# is hard

--- day11/Program.cs
+++ day11/Program.cs
@@ -1,2 +1,20 @@
 ﻿// See https://aka.ms/new-console-template for more information
 Console.WriteLine("Hello, World!");
+
+string[] lines = File.ReadAllLines("/Users/nicholasbilsborrow/Code/nickb14/AdventOfCode2021/day11/input.txt");
+
+int[,] grid = new int[lines.Length, lines[0].Length];
+
+int i = 0;
+foreach (string line in lines)
+{
+    int[] nums = Array.ConvertAll(line.ToCharArray(), c => (int)Char.GetNumericValue(c));
+    
+    int j = 0;
+    foreach (int num in nums)
+    {
+        grid[i, j] = num;
+        j++;
+    }
+    i++;
+}
